d2f174bb58d9ae844097d3792346edef1258566d
Enoch Garoli <eenkiga@gmail.com>
2022-01-08 12:31:04 +0300

    finals

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,7 +12,9 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    data = ModalRoute.of(context)!.settings.arguments as Map;
+    data = data.isNotEmpty
+        ? data
+        : ModalRoute.of(context)!.settings.arguments as Map;
     print(data);
 
     //setBackground
@@ -34,8 +36,17 @@ class _HomeState extends State<Home> {
             children: [
               TextButton.icon(
                   style: TextButton.styleFrom(primary: Colors.grey[300]),
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/location');
+                  onPressed: () async {
+                    dynamic result =
+                        await Navigator.pushNamed(context, '/location');
+                    setState(() {
+                      data = {
+                        'time': result['time'],
+                        'location': result['location'],
+                        'isDaytime': result['isDaytime'],
+                        'flag': result['flag']
+                      };
+                    });
                   },
                   icon: const Icon(Icons.edit_location),
                   label: const Text('Edit Location')),
